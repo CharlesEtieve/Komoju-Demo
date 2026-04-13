8d4d0c698bbe058c07a023f79a7c1a3d2a1449c5
Charles Etieve <charles.etieve@gmail.com>
2026-04-13 10:57:16 +0900

    Add payment failed screen

---
+++ app/src/main/java/com/komoju/demo/presentation/paymentFailed/PaymentFailedScreen.kt
@@ -0,0 +1,34 @@
+package com.komoju.demo.presentation.paymentFailed
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun PaymentFailedScreen(
+    onGoBack: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Payment failed!", fontSize = 24.sp)
+
+        Spacer(Modifier.height(24.dp))
+
+        Button(onClick = onGoBack) {
+            Text("Done")
+        }
+    }
+}
